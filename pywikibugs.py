6fd6b8b16a381689524ffcc9f711fae7579fdd70
Merlijn van Deen <valhallasw@gmail.com>
2014-04-22 17:50:27 +0200

    + #wikimedia-qa

--- pywikibugs.py
+++ pywikibugs.py
@@ -49,6 +49,9 @@ channels = {"##valhallasw": (lambda x: True, {}),
                                 {}),
             "#mediawiki-visualeditor": (lambda x: x.get("X-Bugzilla-Product", None) in ["VisualEditor", "OOjs", "OOjs UI"],
                                         {}),
+            "#wikimedia-qa": (lambda x: (x.get("X-Bugzilla-Product", None) == "Wikimedia") and \
+                                        (x.get("X-Bugzilla-Component", None) in ["Continuous integration", "Quality Assurance"]),
+                              {}),
 }
     
 def send_messages(bot, parsed_email):
