af877a4c1eae029ef6cc3e2b5f772574ed057b1a
Merlijn van Deen <valhallasw+tools@gmail.com>
2014-04-24 11:31:33 +0000

    Bug 49342: Use bugs.python.org/21315 patch to solve q-char encoding issues

--- bzparser.py
+++ bzparser.py
@@ -5,10 +5,9 @@ import time
 
 # Monkey patch email parser to work around crappy bugzilla emails
 # (=?... is only allowed after whitespace)
+from get_unstructured import get_unstructured
+
 import email._header_value_parser, email.headerregistry
-def get_unstructured(value):
-    value = value.replace("=?UTF-8?Q?=20", " =?UTF-8?Q?")
-    return email._header_value_parser.get_unstructured(value)
 email.headerregistry.UnstructuredHeader.value_parser = staticmethod(get_unstructured)
 
 class ParseError(Exception):
