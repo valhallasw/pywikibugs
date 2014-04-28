88c1aa178041e9e427c93e2eaed692d6e0a2a3a0
YuviPanda <yuvipanda@gmail.com>
2014-04-28 15:38:01 +0530

    Move channels config to their own file

--- pywikibugs.py
+++ pywikibugs.py
@@ -14,6 +14,7 @@ import logging
 import irc3
 
 from config import irc_password
+from channels import channels
 
 import subprocess
 try:
@@ -53,26 +54,6 @@ def parse_email(mail):
     done = yield from asyncio.wait_for(fixup_future, timeout=30)
     
     return bep.result
-
-channels = {"#huggle": (lambda x: x.get("X-Bugzilla-Product", None) == "Huggle", {}),
-            "#pywikipediabot": (lambda x: x.get("X-Bugzilla-Product", None) == "Pywikibot", {}),
-            "#wikimedia-corefeatures": (lambda x: (x.get("X-Bugzilla-Product", None) == "MediaWiki extensions") and \
-                                        (x.get("X-Bugzilla-Component", None) in ["Echo", "Flow", "LiquidThreads", "PageCuration", "Thanks", "WikiLove"]),
-                                        {}),
-            "#wikimedia-dev": (lambda x: True, {}),
-            "#wikimedia-labs": (lambda x: x.get("X-Bugzilla-Product", None) in ["Tool Labs tools","Wikimedia Labs"], {}),
-            "#wikimedia-mobile": (lambda x: x.get("X-Bugzilla-Product", None) in ["Wikimedia Mobile", "Commons App", "Wikipedia App", "MobileFrontend"], {}),
-            "#wikimedia-qa": (lambda x: (x.get("X-Bugzilla-Product", None) == "Wikimedia") and \
-                                        (x.get("X-Bugzilla-Component", None) in ["Continuous integration", "Quality Assurance"]),
-                              {}),
-            "#mediawiki-feed": (lambda x: True, {}),
-            "#mediawiki-visualeditor": (lambda x: x.get("X-Bugzilla-Product", None) in ["VisualEditor", "OOjs", "OOjs UI"] or \
-                                        (
-                                            (x.get("X-Bugzilla-Product", None) == "MediaWiki extensions") and \
-                                            (x.get("X-Bugzilla-Component", None) in ["TemplateData"])
-                                        ), {}),
-            "#mediawiki-parsoid": (lambda x: x.get("X-Bugzilla-Product", None) in ["Parsoid"], {}),
-}
 
 def is_gerrit_change(parsed_email):
     return parsed_email["email"] == "gerritadmin@wikimedia.org"
